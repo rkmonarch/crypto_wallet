a9af34a36446dbe01bade481bcc579a2795d534a
Rahul Kulkarni <rahulkulkarni5234@gmail.com>
2023-06-06 21:12:14 +0530

    Added wallet generate functions

---
+++ lib/provider/wallet_provider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:web3dart/web3dart.dart';
+import 'package:bip39/bip39.dart' as bip39;
+import 'package:ed25519_hd_key/ed25519_hd_key.dart';
+import 'package:hex/hex.dart';
+
+abstract class WalletAddressService {
+  String generateMemonics();
+  Future<String> getPrivateKey(String mnemonic);
+  Future<EthereumAddress> getPublicKey(String privateKey);
+}
+
+class WalletProvider extends ChangeNotifier implements WalletAddressService {
+  @override
+  String generateMemonics() {
+    return bip39.generateMnemonic();
+  }
+
+  @override
+  Future<String> getPrivateKey(String mnemonic) async {
+    final seed = bip39.mnemonicToSeed(mnemonic);
+    final master = await ED25519_HD_KEY.getMasterKeyFromSeed(seed);
+    final privateKey = HEX.encode(master.key);
+    return privateKey;
+  }
+
+  @override
+  Future<EthereumAddress> getPublicKey(String privateKey) async {
+    final privatKey = EthPrivateKey.fromHex(privateKey);
+    final address = await privatKey.address;
+    return address;
+  }
+}
