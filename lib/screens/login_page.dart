5281fafcbd19f6f074fefaf468cae458076c1281
Rahul Kulkarni <rahulkulkarni5234@gmail.com>
2023-06-10 12:02:26 +0530

    added some UI changes

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,46 +1,72 @@
 import 'package:crypto_wallet/resources/ui_helpers.dart';
 import 'package:crypto_wallet/screens/generate_mnemonic_page.dart';
 import 'package:crypto_wallet/screens/import_wallet.dart';
+import 'package:crypto_wallet/screens/wallet_page.dart';
 import 'package:crypto_wallet/widgets/button.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String? balance;
+  Future<void> getBalance() async {
+    SharedPreferences preferences =
+        await SharedPreferences.getInstance() as SharedPreferences;
+    setState(() {
+      balance = preferences.getString('balance');
+    });
+  }
+
+  @override
+  void initState() {
+    getBalance();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-        title: Text("Crypto Wallet"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Button(
-              width: deviceWidth(context) / 1.5,
-              ontap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => GenerateMnemonicPage()));
-              },
-              text: "Generate Wallet",
+    return balance == null
+        ? Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              centerTitle: true,
+              title: Text("Crypto Wallet"),
             ),
-            vSpaceMedium(context),
-            Button(
-              width: deviceWidth(context) / 1.5,
-              ontap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ImportWallet()));
-              },
-              text: "Import from SEED",
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Button(
+                    width: deviceWidth(context) / 1.5,
+                    ontap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => GenerateMnemonicPage()));
+                    },
+                    text: "Generate Wallet",
+                  ),
+                  vSpaceMedium(context),
+                  Button(
+                    width: deviceWidth(context) / 1.5,
+                    ontap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ImportWallet()));
+                    },
+                    text: "Import from SEED",
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-    );
+          )
+        : WalletPage();
   }
 }
--- lib/screens/wallet_page.dart
+++ lib/screens/wallet_page.dart
@@ -5,6 +5,7 @@ import 'package:crypto_wallet/provider/wallet_provider.dart';
 import 'package:crypto_wallet/resources/ui_helpers.dart';
 import 'package:crypto_wallet/screens/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:web3dart/web3dart.dart';
 
@@ -17,7 +18,7 @@ class WalletPage extends StatefulWidget {
 
 class _WalletPageState extends State<WalletPage> {
   String walletAddress = '';
-  String balance = '';
+  String? balance;
   String pvKey = '';
 
   Future<void> loadWalletData() async {
@@ -39,9 +40,17 @@ class _WalletPageState extends State<WalletPage> {
     final provider = MoralisProvider();
     final _balance = await provider.getBalance(walletAddress);
     var data = jsonDecode(_balance);
+    SharedPreferences preferences = await SharedPreferences.getInstance();
     setState(() {
       balance = (int.parse(data['balance']) / 1000000000000000000).toString();
+      preferences.setString('balance', balance!);
     });
+  }
+
+  void copyAddress() {
+    Clipboard.setData(ClipboardData(text: walletAddress));
+    final snackBar = SnackBar(content: Text('Copied to Clipboard'));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
   @override
@@ -55,116 +64,149 @@ class _WalletPageState extends State<WalletPage> {
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         centerTitle: true,
-        title: Text("Wallet Page"),
+        title: Text("Wallet"),
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: deviceWidth(context) * 0.1),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            vSpaceMassive(context),
-            // Card(
-            //   color: Color.alphaBlend(Color(0xff614385), Color(0xff516395)),
-            //   child:
-            Column(
-              children: [
-                Center(
-                  child: Text(
-                    "Wallet Address",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                vSpaceSmall(context),
-                Text(
-                  walletAddress,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 17),
-                  softWrap: false,
-                  maxLines: 3,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                vSpaceMedium(context),
-                Center(
-                  child: Text(
-                    "Balance",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                vSpaceSmall(context),
-                Center(
-                    child: Text(
-                  balance,
-                  style: TextStyle(fontSize: 17),
-                )),
-                SizedBox(
-                  height: deviceHeight(context) * 0.1,
-                ),
-              ],
-            ),
-            // ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                CircleAvatar(
-                  radius: 25,
-                  child: Icon(Icons.send),
-                ),
-                CircleAvatar(
-                  radius: 25,
-                  child: Icon(Icons.refresh),
-                )
-              ],
-            ),
-            vSpaceSmall(context),
-            vSpaceSmall(context),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [Text("Send"), Text("Refresh")],
-            ),
-            vSpaceMedium(context),
-            vSpaceMedium(context),
-            Expanded(
-                child: DefaultTabController(
-                    length: 3,
-                    child: Column(
-                      children: [
-                        TabBar(labelColor: Colors.black, tabs: [
-                          Text("Assets"),
-                          Text("NFTs"),
-                          Text("Options")
-                        ]),
-                        Expanded(
-                            child: TabBarView(
+      body: balance != null
+          ? Padding(
+              padding:
+                  EdgeInsets.symmetric(horizontal: deviceWidth(context) * 0.1),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  vSpaceMassive(context),
+
+                  Column(
+                    children: [
+                      Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            ListTile(
-                              title: Text("data"),
-                              leading: Icon(Icons.currency_bitcoin),
+                            Text(
+                              "Wallet Address",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
                             ),
-                            Text("data333"),
-                            Center(
-                              child: ListTile(
-                                leading: Icon(Icons.logout),
-                                title: Text("Logout"),
-                                onTap: () async {
-                                  SharedPreferences preferences =
-                                      await SharedPreferences.getInstance();
-                                  await preferences.remove("privateKey");
-                                  //push to login page
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (builder) => LoginPage()));
+                            hSpaceMedium(context),
+                            IconButton(
+                                onPressed: () {
+                                  copyAddress();
                                 },
-                              ),
-                            )
+                                icon: Icon(Icons.copy))
                           ],
-                        ))
-                      ],
-                    )))
-          ],
-        ),
-      ),
+                        ),
+                      ),
+                      vSpaceSmall(context),
+                      Text(
+                        walletAddress,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 17),
+                        softWrap: false,
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      vSpaceMedium(context),
+                      Center(
+                        child: Text(
+                          "Balance",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      vSpaceSmall(context),
+                      Center(
+                          child: Text(
+                        balance ?? "",
+                        style: TextStyle(fontSize: 17),
+                      )),
+                      SizedBox(
+                        height: deviceHeight(context) * 0.1,
+                      ),
+                    ],
+                  ),
+                  // ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      CircleAvatar(
+                        radius: 25,
+                        child: Icon(Icons.send),
+                      ),
+                      CircleAvatar(
+                        radius: 25,
+                        child: Icon(Icons.refresh),
+                      )
+                    ],
+                  ),
+                  vSpaceSmall(context),
+                  vSpaceSmall(context),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [Text("Send"), Text("Refresh")],
+                  ),
+                  vSpaceMedium(context),
+                  vSpaceMedium(context),
+                  Expanded(
+                      child: DefaultTabController(
+                          length: 3,
+                          child: Column(
+                            children: [
+                              TabBar(
+                                  labelPadding: EdgeInsets.symmetric(
+                                      vertical: deviceHeight(context) * 0.01,
+                                      horizontal: deviceWidth(context) * 0.05),
+                                  labelColor: Colors.black,
+                                  tabs: [
+                                    Text("Assets"),
+                                    Text("NFTs"),
+                                    Text("Options")
+                                  ]),
+                              Expanded(
+                                  child: TabBarView(
+                                children: [
+                                  ListView(children: <Widget>[
+                                    vSpaceSmall(context),
+                                    Card(
+                                      child: ListTile(
+                                        leading: Image.asset(
+                                          "assets/images/polygon.png",
+                                          height: 40,
+                                        ),
+                                        title: Text(
+                                            balance != null ? balance! : ""),
+                                      ),
+                                    ),
+                                  ]),
+                                  Text("data333"),
+                                  Center(
+                                    child: ListTile(
+                                      leading: Icon(Icons.logout),
+                                      title: Text("Logout"),
+                                      onTap: () async {
+                                        SharedPreferences preferences =
+                                            await SharedPreferences
+                                                .getInstance();
+                                        await preferences.remove("privateKey");
+                                        //push to login page
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (builder) =>
+                                                    LoginPage()));
+                                      },
+                                    ),
+                                  )
+                                ],
+                              ))
+                            ],
+                          )))
+                ],
+              ),
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
     );
   }
 }
